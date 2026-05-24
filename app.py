45720ad43b513ee8231f244d7869612d9aff6026
Joseplexflo <jlezmaf@gmail.com>
2026-05-24 11:01:56 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import numpy as np
+import libreria_funciones as lf
+
+st.title("Mi primera aplicación en Python")
+st.sidebar.title("Parámetros")
+st.write("Elaborado por: Jhonattan Lezma F.")
