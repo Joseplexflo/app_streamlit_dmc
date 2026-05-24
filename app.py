769ea68657d63292027defbffd5efbf67d1c2c81
Joseplexflo <jlezmaf@gmail.com>
2026-05-24 11:31:50 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,26 +3,28 @@ import numpy as np
 
 st.title("APLICACIÓN EN STREAMLIT")
 st.write("Elaborado por: Jhonattan Josep Lezma Florida")
-st.write("Módulo 1 – Python Fundamentals")
-st.write("Ingeniero jefe de proyectos en empresa de exportación textil")
-st.write("2026")
-st.write("Este proyecto es el desarrollo de lo aprendio en el Módulo 1 – Python Fundamentals dictado por DMC")
-st.write("Tecnologías utilizadas: librerias numpy,")
 
 st.sidebar.title("Ejercicios")
 st.sidebar.image("logo_personal.png")
 
-sesion = st.sidebar.selectbox("Seleccione una ejercicios", ["Ejercicio 1","Ejercicio 2","Ejercicio 3","Ejercicio 4"])
+item = st.sidebar.selectbox("Seleccione un item:", ["Home","Ejercicio 1","Ejercicio 2","Ejercicio 3","Ejercicio 4"])
 
-if sesion == "Ejercicio 1":
+if item == "Home":
+  st.write("Módulo 1 – Python Fundamentals")
+  st.write("Ingeniero jefe de proyectos en empresa de exportación textil")
+  st.write("2026")
+  st.write("Este proyecto es el desarrollo de lo aprendio en el Módulo 1 – Python Fundamentals dictado por DMC")
+  st.write("Tecnologías utilizadas: librerias numpy,")
+
+elif item == "Ejercicio 1":
   st.write("Flujo de caja con listas")
   
   
-elif sesion == "Ejercicio 2":
+elif item == "Ejercicio 2":
   st.write("Registro con NumPy, arrays y DataFrame")
     
   
-elif sesion == "Ejercicio 3":
+elif item == "Ejercicio 3":
   st.write("Uso de funciones desde una librería externa")
     
 
