80da652435e1397281c2c2c08862b91f52acbd40
Joseplexflo <jlezmaf@gmail.com>
2026-05-24 13:09:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ import numpy as np
 import pandas as pd
 import libreria_funciones as lf
 
-from libreria_funciones_proyecto1 import flujo_caja_neto, precio_venta_final, produccion_real_linea, LoteProduccionTextil
+from libreria_funciones_proyecto1 import flujo_caja_neto, precio_venta_final, produccion_real_linea
+from libreria_clases_proyecto1 import LoteProduccionTextil
 
 st.title("APLICACIÓN EN STREAMLIT")
 st.write("Elaborado por: **Jhonattan Josep Lezma Florida**")
