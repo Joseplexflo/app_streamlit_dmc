311e0e72881cc0eb00b786bcd6fca9b9cf729111
Joseplexflo <jlezmaf@gmail.com>
2026-05-24 13:05:47 -0500

    Update libreria_funciones_proyecto1.py

--- libreria_funciones_proyecto1.py
+++ libreria_funciones_proyecto1.py
@@ -21,3 +21,24 @@ def produccion_real_linea(minutos_disponibles, eficiencia_pct, tiempo_estandar, 
     produccion_neta = produccion_bruta * (1 - defectos_pct / 100)
     
     return round(produccion_neta, 2)
+
+
+
+class LoteProduccionTextil:
+    def __init__(self, id_lote, tipo_tela, color, cantidad_metros, estado="Programado"):
+        """
+        Clase que representa un lote de producción en la planta textil.
+        """
+        self.id_lote = id_lote
+        self.tipo_tela = tipo_tela
+        self.color = color
+        self.cantidad_metros = float(cantidad_metros)
+        self.estado = estado  # Ej. Programado, En teñido, En corte, Terminado
+
+    def actualizar_estado(self, nuevo_estado):
+        """Método para actualizar el estado del lote."""
+        self.estado = nuevo_estado
+
+    def actualizar_cantidad(self, nueva_cantidad):
+        """Método para corregir la cantidad de metros del lote."""
+        self.cantidad_metros = float(nueva_cantidad)
